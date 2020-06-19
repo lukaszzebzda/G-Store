208f5257171de795c94186a55bc39b7a48628475
Lukixs <zeblukasz@gmail.com>
2020-06-19 20:54:37 +0200

    Zmodyfikowano: Dokumentacja

---
+++ sql/SKLEP_PRZEDMIOTY.sql
@@ -0,0 +1,5 @@
+INSERT INTO SKLEP.PRZEDMIOTY (PRZE_ID, PRZE_NAZWA, PRZE_OPIS, PRZE_CENA, PRZE_IMAGE, KATE_ID, PRZE_CRITIC_RATE) VALUES (204, 'Grand Thef Auto V', 'Komputerowa gra akcji nalezaca do serii Grand Theft Auto', 55, 'gta_V.jpg', 201, 9);
+INSERT INTO SKLEP.PRZEDMIOTY (PRZE_ID, PRZE_NAZWA, PRZE_OPIS, PRZE_CENA, PRZE_IMAGE, KATE_ID, PRZE_CRITIC_RATE) VALUES (205, 'Counter Strike Global Offensive', 'Gra komputerowa typu shooter, polegajaca na glownie na refleksie, zrecznosci oraz zmyslach taktycznych gracza.', 55, 'CSGO.jfif', 202, 10);
+INSERT INTO SKLEP.PRZEDMIOTY (PRZE_ID, PRZE_NAZWA, PRZE_OPIS, PRZE_CENA, PRZE_IMAGE, KATE_ID, PRZE_CRITIC_RATE) VALUES (206, 'Assasin Creed II', 'Gra o Assasynach', 35, 'assassinscreed2.jpg', 201, 8);
+INSERT INTO SKLEP.PRZEDMIOTY (PRZE_ID, PRZE_NAZWA, PRZE_OPIS, PRZE_CENA, PRZE_IMAGE, KATE_ID, PRZE_CRITIC_RATE) VALUES (207, 'Minecraft', 'Gra o zwiedzaniu swiata', 60, 'minecraft.jpg', 200, 8);
+INSERT INTO SKLEP.PRZEDMIOTY (PRZE_ID, PRZE_NAZWA, PRZE_OPIS, PRZE_CENA, PRZE_IMAGE, KATE_ID, PRZE_CRITIC_RATE) VALUES (221, 'Skyrim', 'Gra RPG', 45, 'skyrim.jpg', 200, 10);
